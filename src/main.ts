67188e375041836be9c05d584cd15b50b10b2f6d
Jack Hodgkiss <52131498+jackhodgkiss@users.noreply.github.com>
2022-02-24 18:29:15 +0000

    feat: add generation primes by trial division

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
+function* generate_prime_numbers(): Generator<number> {
+    let potential_prime: number = 2;
+    if(potential_prime == 2) { yield 2; }
+    if(potential_prime % 2 == 0) { potential_prime += 1; }
+    while(true) {
+        let is_prime: Boolean = true;
+        for (let counter = 3; counter < Math.sqrt(potential_prime); counter += 2) {
+            if(potential_prime % counter == 0) { is_prime = false; break; }
+        }
+        if(is_prime) { yield potential_prime; }
+        potential_prime += 2;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Hello, World!");
+    for(let prime_number of generate_prime_numbers()) {
+        if(prime_number > 1000) { break; }
+        console.log(prime_number);
+    }
 });
