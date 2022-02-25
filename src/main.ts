273f6055403aced4218eed31073764fa094eb5e8
Jack Hodgkiss <52131498+jackhodgkiss@users.noreply.github.com>
2022-02-25 00:18:28 +0000

    feat: add plan to the top of main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
+// 1. Construct grid that spirals out from the center of the canvas.
+
+// 2. Allow for `cells` to displayed within the canvas.
+
+// 3. Alter the visual appearance of `cells` if they meet certain criteria (prime, mersenne prime)
+
 class Application {
     constructor() {
         console.log("Hello, World!");
