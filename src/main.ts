5ad57c8fad8594f6810a751ec3daddd332ead848
Jack Hodgkiss <52131498+jackhodgkiss@users.noreply.github.com>
2022-02-28 10:45:54 +0000

    feat: add basic cell class

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,33 @@
 
 // 3. Alter the visual appearance of `cells` if they meet certain criteria (prime, mersenne prime)
 
+class Cell {
+    is_prime: boolean;
+    abscissa: number;
+    ordinate: number;
+    constructor(is_prime: boolean, abscissa: number, ordinate: number) {
+        this.is_prime = is_prime;
+        this.abscissa = abscissa;
+        this.ordinate = ordinate;
+    }
+
+    draw(context: CanvasRenderingContext2D): void {
+        context.rect(this.abscissa, this.ordinate, 50, 50);
+        context.fill();
+    }
+}
+
 class Application {
     canvas: HTMLCanvasElement;
     context: CanvasRenderingContext2D;
     constructor() {
         this.canvas = document.getElementById("application-canvas") as HTMLCanvasElement;
         this.context = this.canvas.getContext("2d") as CanvasRenderingContext2D;
-        this.canvas.setAttribute("style", "background-color: black;");
+        this.draw();
+    }
+    draw(): void {
+        const cell = new Cell(false, 50, 50);
+        cell.draw(this.context);
     }
 }
 
