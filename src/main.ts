8e1509db5ea7697f6bce75f5cd65ea983545afb0
Jack Hodgkiss <52131498+jackhodgkiss@users.noreply.github.com>
2022-03-06 17:03:14 +0000

    feat: add support for display selected value

--- src/main.ts
+++ src/main.ts
@@ -31,6 +31,7 @@ class Cell {
 class Application {
     canvas: HTMLCanvasElement;
     context: CanvasRenderingContext2D;
+    canvas_size: number = 600;
     cell_size: number;
     row_offset: number;
     column_offset: number;
@@ -45,7 +46,7 @@ class Application {
         this.register_event_listeners();
     }
 
-    coordinates_to_value(row: number, column: number): number {
+    static coordinates_to_value(row: number, column: number): number {
         let value: number = 0;
         const layer: number = Math.max(Math.abs(row), Math.abs(column));
         const smallest_layer_element: number = Math.pow(3 + (layer - 2) * 2, 2) + 1;
@@ -63,7 +64,21 @@ class Application {
     }
 
     on_click(event: MouseEvent): void {
-        console.log(event.offsetX);
+        const cell_size_field = document.getElementById("cell-size") as HTMLInputElement;
+        const row_field = document.getElementById("row") as HTMLInputElement;
+        const column_field = document.getElementById("column") as HTMLInputElement;
+        const cell_size: number= parseInt(cell_size_field.value);
+        const row_offset: number = parseInt(row_field.value);
+        const column_offset: number = parseInt(column_field.value);
+        const half_canvas_cell: number = Math.floor((600 / cell_size) / 2);
+        const selected_row = (Math.floor(event.offsetY / cell_size) - half_canvas_cell) + row_offset;
+        const selected_row_span = document.getElementById("selected-row") as HTMLSpanElement;
+        selected_row_span.textContent = `Selected Row: ${selected_row}`;
+        const selected_column = (Math.floor(event.offsetX / cell_size) - half_canvas_cell) + column_offset;
+        const selected_column_span = document.getElementById("selected-column") as HTMLSpanElement;
+        selected_column_span.textContent = `Selected Column: ${selected_column}`;
+        const selected_value_span = document.getElementById("selected-value") as HTMLSpanElement;
+        selected_value_span.textContent = `Selected Value: ${Application.coordinates_to_value(selected_row, selected_column)}`
     }
 
     unregister_event_listeners() {
@@ -87,14 +102,13 @@ class Application {
 
 
     draw(): void {
-        const canvas_size: number = 600;
-        const half_canvas_cell: number = Math.floor((canvas_size / this.cell_size) / 2);
-        this.context.clearRect(0, 0, canvas_size, canvas_size);
+        const half_canvas_cell: number = Math.floor((this.canvas_size / this.cell_size) / 2);
+        this.context.clearRect(0, 0, this.canvas_size, this.canvas_size);
         this.context.save();
-        this.context.translate(canvas_size / 2, canvas_size / 2);
+        this.context.translate(this.canvas_size / 2, this.canvas_size / 2);
         for (let row = this.row_offset + (half_canvas_cell * -1); row <= this.row_offset + half_canvas_cell; row++) {
             for (let column = this.column_offset + (half_canvas_cell * - 1); column <= this.column_offset + half_canvas_cell; column++) {
-                const value = this.coordinates_to_value(row, column);
+                const value = Application.coordinates_to_value(row, column);
                 const is_prime: boolean = this.is_prime(value);
                 new Cell(is_prime, (column - this.column_offset) * this.cell_size, (row - this.row_offset) * this.cell_size, this.cell_size).draw(this.context);
             }
